59c632968be3e0e0a167cd503798bb0e0746eb3b
udaykiran199 <udaykiran199@gmail.com>
2019-05-07 23:15:35 +0530

    master1 1st commit

--- SimpleServlet.java
+++ SimpleServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class SimpleServlet hii  dgdgdgsd gdssdgs
+ * Servlet implementation class SimpleServlet hii  dgdgdgsd gdssdgs uday
  */
 @WebServlet("/SimpleServlet")
 public class SimpleServlet extends HttpServlet {
