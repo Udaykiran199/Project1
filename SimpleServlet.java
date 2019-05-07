e3745e4cdd1163039b838a6760b96e40d5ce3360
udaykiran199 <udaykiran199@gmail.com>
2019-05-07 22:02:09 +0530

    first commit

---
+++ SimpleServlet.java
@@ -0,0 +1,32 @@
+package com.uday.javademo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/SimpleServlet")
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stubs 
+		System.out.println("This is doget method");
+		
+		PrintWriter writer= response.getWriter();
+		
+		writer.println("<h3> Hello </h3>");
+		
+	}
+
+	
+
+}
